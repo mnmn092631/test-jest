01f6151f8ac887bc1311deb3a9cff1d03018a164
mnmn092631 <mnmn092631@gmail.com>
2025-02-09 20:32:54 +0900

    if문 없을 때 테스트 커버리지 100% 만들기

---
+++ ch2/models/user.test.js
@@ -0,0 +1,21 @@
+const User = require("./user");
+
+describe("user", () => {
+  it("initiate가 정상 작동한다", () => {
+    const fn = jest.spyOn(User, "init").mockImplementation();
+    User.initiate({});
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it("associate가 정상 작동한다", () => {
+    const db = {
+      User: {
+        hasMany: jest.fn(),
+        belongsToMany: jest.fn(),
+      },
+    };
+    User.associate(db);
+    expect(db.User.hasMany).toHaveBeenCalledTimes(1);
+    expect(db.User.belongsToMany).toHaveBeenCalledTimes(2);
+  });
+});
